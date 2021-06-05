321866fbf6b4e5ba5162c940ad737f017832531d
John Riordan <iam.john.riordan@gmail.com>
2021-06-04 20:06:40 -0700

    Light design touches. Remove bonuses transition

--- src/lib/Attributes/index.svelte
+++ src/lib/Attributes/index.svelte
@@ -18,15 +18,18 @@
 
 <div class="container">
 	<div class="left">
-		<h2 class="mask points-available" class:canAllocate={$attrs.available}>{$attrs.available}</h2>
+		<div class="points">
+			<div class="mask2 compass" />
+			<h2 class="mask points-available" class:canAllocate={$attrs.available}>{$attrs.available}</h2>
+		</div>
 		<h4 class="subtitle">Points <br />Available</h4>
 		<button class="subtle respec" class:disabled={!$attrs.anyPtsAllocated} on:click={respec}
 			>Respec</button
 		>
-		<h2 class="bonuses-title">Bonuses:</h2>
+		<h2 class="mask2 bonuses-title">Bonuses:</h2>
 		<ul class="bonuses">
 			{#each $attrs.bonuses as bonus, i}
-				<li transition:fly={{ x: -10, duration: 250, delay: i * 150 }}>
+				<li>
 					{@html bonus}
 				</li>
 			{/each}
@@ -51,8 +54,51 @@
 		flex: 1;
 		text-align: center;
 	}
+
+	.points {
+		position: relative;
+	}
+	.compass,
+	.compass::before,
+	.compass::after {
+		content: '';
+		position: absolute;
+		aspect-ratio: 1;
+		width: 200px;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+	.compass::before,
+	.compass::after {
+		top: 0;
+		left: 0;
+	}
+
+	@keyframes spin {
+		to {
+			transform: rotate(1turn);
+		}
+	}
+	.compass {
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		background-image: url('/static/compass-diamond.png');
+	}
+
+	.compass::before {
+		opacity: 0.3;
+		background-image: url('/static/compass-outer.png');
+		animation: spin 40s linear forwards infinite;
+	}
+	.compass::after {
+		opacity: 0.4;
+		background-image: url('/static/compass-ring.png');
+		animation: spin 30s linear reverse infinite;
+	}
+
 	.points-available {
-		font-size: 8.5rem;
+		font-size: 9rem;
 		line-height: 1;
 		letter-spacing: 0;
 		text-shadow: 0 10px 28px var(--black);
@@ -73,8 +119,23 @@
 	}
 
 	.bonuses-title {
+		position: relative;
 		text-align: left;
 		margin: 3rem 0 0;
+		padding-bottom: 0.5rem;
+		font-size: 1.25rem;
+	}
+	.bonuses-title:after {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 2px;
+		background-image: url(https://d2lchq0n03yu65.cloudfront.net/statics/2021-05-20/images/new-world-sprites.png);
+		background-repeat: no-repeat;
+		background-position: 66% 74%;
+		-webkit-mask-image: linear-gradient(to right, black 0%, black 30%, black 70%, transparent 100%);
 	}
 	.bonuses {
 		text-align: left;
@@ -82,33 +143,15 @@
 		margin: 0;
 		padding: 0;
 	}
+	.bonuses::-webkit-scrollbar {
+		display: none;
+	}
 	.bonuses li {
 		position: relative;
 		margin: 1rem 0;
-		padding-bottom: 1rem;
-	}
-
-	.bonuses li:after {
-		content: '';
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		width: 100%;
-		height: 2px;
-		background-image: url('https://d2lchq0n03yu65.cloudfront.net/statics/2021-05-20/images/new-world-sprites.png');
-		background-repeat: no-repeat;
-		background-position: 92% 74%;
-		-webkit-mask-image: linear-gradient(
-			to right,
-			transparent 0%,
-			black 30%,
-			black 70%,
-			transparent 100%
-		);
 	}
 
 	.bonuses li :global(span) {
-		/* font-weight: 700; */
 		color: var(--yellow);
 	}
 
