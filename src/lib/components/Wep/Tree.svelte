d943a762137e5d427ac229ca112409095fba8fe9
John Riordan <iam.john.riordan@gmail.com>
2021-07-14 19:17:19 -0700

    skill tree bg

--- src/lib/components/Wep/Tree.svelte
+++ src/lib/components/Wep/Tree.svelte
@@ -1,59 +1,70 @@
 <script>
-	import LineBorder from '$lib/components/LineBorder/index.svelte';
-	import Skill from './Skill.svelte';
+  import LineBorder from '$lib/components/LineBorder/index.svelte';
+  import Skill from './Skill.svelte';
 
-	const size = {
-		cols: 5,
-		rows: 6
-	};
+  const size = {
+    cols: 5,
+    rows: 6
+  };
 
-	export let tree;
-	export let store;
-	export let ptsRemaining;
-	export let treeIndex;
+  export let tree;
+  export let store;
+  export let ptsRemaining;
+  export let treeIndex;
 
-	function addSkill(event) {
-		if (store) store.addSkill(event.detail.skill);
-	}
-	function removeSkill(event) {
-		if (store) store.removeSkill(event.detail.skill);
-	}
+  function addSkill(event) {
+    if (store) store.addSkill(event.detail.skill);
+  }
+  function removeSkill(event) {
+    if (store) store.removeSkill(event.detail.skill);
+  }
+
+  $: maxRow = $store && $store.rows.length ? Math.max(...$store.rows) : 0;
 </script>
 
 <div>
-	<h2 class="tree-name">{tree.name}</h2>
-	<LineBorder>
-		<div class="tree-grid" style="--tree-color: var({tree.color})">
-			{#each Array(size.rows) as _, row}
-				{#each Array(size.cols) as _, col}
-					<Skill
-						{store}
-						{ptsRemaining}
-						skillInfo={tree && tree.skills[`${row + 1}_${col + 1}`]}
-						row={row + 1}
-						col={col + 1}
-						tooltipSide={treeIndex === 1 ? 'left' : 'right'}
-						on:addSkill={addSkill}
-						on:removeSkill={removeSkill}
-					/>
-				{/each}
-			{/each}
-		</div>
-	</LineBorder>
+  <h2 class="tree-name">{tree.name}</h2>
+  <LineBorder>
+    <div class="backdrop-fill" style={`--y-axis-count: ${maxRow};`} />
+    <div class="tree-grid" style="--tree-color: var({tree.color})">
+      {#each Array(size.rows) as _, row}
+        {#each Array(size.cols) as _, col}
+          <Skill
+            {store}
+            {ptsRemaining}
+            skillInfo={tree && tree.skills[`${row + 1}_${col + 1}`]}
+            row={row + 1}
+            col={col + 1}
+            tooltipSide={treeIndex === 1 ? 'left' : 'right'}
+            on:addSkill={addSkill}
+            on:removeSkill={removeSkill}
+          />
+        {/each}
+      {/each}
+    </div>
+  </LineBorder>
 </div>
 
 <style>
-	.tree-grid {
-		--gap: 0.5rem;
+  .backdrop-fill {
+    position: absolute;
+    height: calc(100% / 6 * var(--y-axis-count));
+    width: 100%;
+    background: linear-gradient(to bottom, transparent, var(--white));
+    opacity: 0.075;
+    transition: height var(--transition);
+  }
+  .tree-grid {
+    --gap: 0.5rem;
 
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-template-rows: repeat(6, 1fr);
-		gap: var(--gap);
-		padding: 1rem;
-	}
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(6, 1fr);
+    gap: var(--gap);
+    /* padding: 1rem; */
+  }
 
-	.tree-name {
-		text-align: center;
-	}
+  .tree-name {
+    text-align: center;
+  }
 </style>
