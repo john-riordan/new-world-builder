9b723ae7f16fc238b5819b73ae90774ab697f8e6
John Riordan <iam.john.riordan@gmail.com>
2021-11-27 14:49:48 -0800

    feat: added calculator routes and fixed attr matrix

---
+++ src/routes/mote-calculator.svelte
@@ -0,0 +1,42 @@
+<script context="module">
+  export const prerender = true;
+
+  import motes from '../motes';
+
+  export async function load() {
+    return {
+      props: {
+        data: {
+          motes
+        }
+      }
+    };
+  }
+</script>
+
+<script>
+  import MoteCard from '$lib/components/MoteCard/index.svelte';
+
+  export let data;
+
+  let refineTax = 1;
+</script>
+
+<svelte:head>
+  <title>Mote Calculator | World Forge</title>
+</svelte:head>
+
+<div class="cards">
+  {#each data.motes as mote}
+    <MoteCard {mote} bind:refineTax />
+  {/each}
+</div>
+
+<style>
+  .cards {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-auto-rows: auto;
+    gap: 1.5rem;
+  }
+</style>
